fc023b86f0b5a7d0c7bdc6a95512362306606d00
Radu Chiriac <radu@zerodawn.tech>
2022-12-27 18:33:24 +0200

    Added new scripts from course

---
+++ read2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Your Name: " NAME
+read -sp "Your age: " AGE
+
+echo
+echo "Name: $NAME, Age: $AGE"
---
+++ read_from_file.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read HOSTNAME < /etc/hostname
+
+echo Hostname: $HOSTNAME
